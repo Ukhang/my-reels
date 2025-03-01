93a5d3931b40f25b05eda967a70c5c06858620ef
Ukhang <ukhangmarma459@gmail.com>
2025-03-01 17:42:50 +0600

    update sidebar design

--- src/app/(profile-layout)/layout.tsx
+++ src/app/(profile-layout)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
   return (
     <div className="relative flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 ml-56 h-screen overflow-hidden">{children}</main>
+      <main className="flex-1 lg:ml-56 ml-[74px] h-screen overflow-hidden">{children}</main>
     </div>
   );
 }
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -41,69 +41,99 @@ const Sidebar = () => {
   };
 
   return (
-    <aside className="w-56 p-4 bg-white border-r flex flex-col fixed top-0 left-0 h-full">
-      <Link
-        href={"/"}
-        className="flex items-center gap-2 font-semibold text-lg px-2 py-2"
-        prefetch={true}
-        onClick={() => toast("Welcome to My Reels!")}
-      >
-        <Image src={"/images/logo.png"} alt="logo" width={30} height={30} />
-        My Reels
-      </Link>
-      <ul className="mt-4 flex flex-col gap-1 pb-6">
-        {sidebarLinks.map((link, index) => (
-          <li key={index} className="flex">
-            <Link
-              href={link.href}
-              className={cn(
-                "flex items-center gap-3 w-full px-2 py-2 rounded-md font-medium text-gray-400 hover:bg-main transition duration-200",
-                {
-                  "text-black": pathname === link.href, // If pathname matches href, text will be black
-                }
-              )}
-            >
-              {link.icon}
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <>
+      <aside className="w-56 p-4 bg-white border-r lg:flex hidden flex-col fixed top-0 left-0 h-full">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 font-semibold text-lg px-2 py-2"
+          prefetch={true}
+          onClick={() => toast("Welcome to My Reels!")}
+        >
+          <Image src={"/images/logo.png"} alt="logo" width={30} height={30} />
+          My Reels
+        </Link>
+        <ul className="mt-4 flex flex-col gap-1 pb-6">
+          {sidebarLinks.map((link, index) => (
+            <li key={index} className="flex">
+              <Link
+                href={link.href}
+                className={cn(
+                  "flex items-center gap-3 w-full px-2 py-2 rounded-md font-medium text-gray-400 hover:bg-main transition duration-200",
+                  {
+                    "text-black": pathname === link.href, // If pathname matches href, text will be black
+                  }
+                )}
+              >
+                {link.icon}
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-      <div className="mt-auto pb-6">
-        {session ? (
-          <AlertDialog>
-            <AlertDialogTrigger asChild className="w-full">
-              <Button>Sign Out</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent className="max-w-sm">
-              <AlertDialogHeader>
-                <AlertDialogTitle>
-                  Are you sure you want to log out?
-                </AlertDialogTitle>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel asChild>
-                  <Button variant={"outline"}>Cancel</Button>
-                </AlertDialogCancel>
-                <AlertDialogAction asChild className="bg-none">
-                  <Button onClick={handleSignOut}>Sign Out</Button>
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        ) : (
-          <Button
-            className="w-full"
-            onClick={() => {
-              router.push("/login");
-            }}
-          >
-            Login
-          </Button>
-        )}
-      </div>
-    </aside>
+        <div className="mt-auto pb-6">
+          {session ? (
+            <AlertDialog>
+              <AlertDialogTrigger asChild className="w-full">
+                <Button>Sign Out</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent className="max-w-sm">
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Are you sure you want to log out?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel asChild>
+                    <Button variant={"outline"}>Cancel</Button>
+                  </AlertDialogCancel>
+                  <AlertDialogAction asChild className="bg-none">
+                    <Button onClick={handleSignOut}>Sign Out</Button>
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          ) : (
+            <Button
+              className="w-full"
+              onClick={() => {
+                router.push("/login");
+              }}
+            >
+              Login
+            </Button>
+          )}
+        </div>
+      </aside>
+
+      <aside className="p-4 bg-white border-r flex items-center lg:hidden flex-col fixed top-0 left-0 h-full">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 font-semibold text-lg"
+          prefetch={true}
+          onClick={() => toast("Welcome to My Reels!")}
+        >
+          <Image src={"/images/logo.png"} alt="logo" width={30} height={30} />
+        </Link>
+        <ul className="mt-4 flex flex-col gap-1 pb-6">
+          {sidebarLinks.map((link, index) => (
+            <li key={index} className="flex">
+              <Link
+                href={link.href}
+                className={cn(
+                  "flex items-center gap-3 w-full p-2 rounded-md font-medium text-gray-400 hover:bg-main transition duration-200",
+                  {
+                    "text-black": pathname === link.href,
+                  }
+                )}
+              >
+                {link.icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </>
   );
 };
 
