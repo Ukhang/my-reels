ef7b9b431fff54b331f0e5262b446f2b0029ce97
Ukhang <ukhangmarma459@gmail.com>
2025-03-01 11:14:48 +0600

    update auth layout

--- src/app/components/auth-header.tsx
+++ src/app/components/auth-header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const AuthHeader = () => {
   return (
-    <header className="w-full bg-white fixed top-0 z-[999] border-b">
+    <header className="w-full fixed top-0 z-[999]">
       <nav className="container mx-auto px-4 sm:py-4 py-3">
         <Link href={"/"} className="flex items-center gap-2 font-semibold text-lg">
           <Image src={"/images/logo.svg"} alt="logo" width={30} height={30} />
