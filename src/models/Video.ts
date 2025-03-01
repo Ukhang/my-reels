1f4e96c116f837f58ea81c646a0c26d9eb74ccc7
Ukhang <ukhangmarma459@gmail.com>
2025-03-01 11:38:15 +0600

    update video.ts file

--- src/models/Video.ts
+++ src/models/Video.ts
@@ -35,6 +35,6 @@ const videoSchema = new Schema<IVideo>(
   { timestamps: true }
 );
 
-const Video = models?.Video || model<IVideo>("video", videoSchema);
+const Video = models.Video || model<IVideo>("Video", videoSchema);
 
 export default Video;
